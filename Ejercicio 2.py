762e628627086a1fdb5724e00cc4b9d2f18d1181
Edgar Mendieta <edgar.fmcascio@gmail.com>
2026-04-06 21:26:06 -0300

    Update Ejercicio 2.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -2,4 +2,19 @@ class Punto():
   def __init__(self,x,y):
     self.x = x
     self.y = y
+  def eje_x(self):
+        return self.x
+
+    def eje_y(self):
+        return self.y
+    
+    def impresion(self):
+        return "el valor de x es= " + str(self.x) + " y el valor de y es = " + str/(self.y)
+  def opuesto(self):
+    return Punto(-self.x, -self.y)
+    
+
+
+    
+    
   
